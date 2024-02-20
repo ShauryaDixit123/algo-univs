b0cfec26142b6e9964affc38a2363c420e3c8d33
Shaurya <shaurya@Shauryas-mac.local>
2024-02-20 18:03:09 +0530

    allowed host

--- djangoPoc/settings/base.py
+++ djangoPoc/settings/base.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY", default="django-insecure$djangoPoc.settings.lo
 
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
